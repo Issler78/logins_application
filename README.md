7d750b6fa120bf4b83d4c150a842f66045b82c2e
issler78 <132786398+Issler78@users.noreply.github.com>
2023-08-11 23:20:40 -0300

    UpdateForScreens

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # logins_application
  Table logins with CRUD in Laravel
+
+<a href="{{ route('logins.index') }}">Ir para a tabela de Logins</a>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,6 @@
 
 @section('content')
     <h1>Home</h1>
+
+    <a href="{{ route('logins.index') }}">Ir para a tabela de logins.</a>
 @endsection
